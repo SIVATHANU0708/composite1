b4e6267e9f33ab4a1d492097b6cb865c997bbb05
SIVATHANU0708 <35222125+SIVATHANU0708@users.noreply.github.com>
2018-03-16 16:54:57 +0530

    composite1.C
    composite or not

---
+++ composite1.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int n,i,count=0;
+printf("enter the number:");
+scanf("%d",&n);
+for(i=2;i<n;i++)
+{
+if(n%i==0)
+{
+count++;
+}
+}
+if(count==0)
+{
+printf("not composite");
+}
+else
+{
+printf("yes composite");
+}
+return 0;
+}
